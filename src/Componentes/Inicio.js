2a3b4aa504744713f233a37c12d95d43494def76
Gary Ortiz <goritz1490@gmail.com>
2020-08-02 19:26:09 -0600

    empezo a trabajar con consumo de get y post

---
+++ src/Componentes/Inicio.js
@@ -0,0 +1,21 @@
+
+
+import axios from 'axios';
+import React, { Component } from 'react'
+
+
+export default class Inicio extends React.Component {
+  state = {
+    per: []
+  }
+
+
+  render() {
+   
+    return (
+      <ul>
+     
+      </ul>
+    )
+  }
+}
