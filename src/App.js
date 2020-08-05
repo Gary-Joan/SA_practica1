d1c127399709cfaefd3343b98020c4a8293d7664
Gary Ortiz <goritz1490@gmail.com>
2020-08-05 07:24:40 -0600

    casi terminado
    falta descripsion del readme y listo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Inicio from './Componentes/Inicio.js';
 
 class App extends Component {
+  //Aqui mandamos a llamar al componente INICIO que sera mostrado
   render() {
     return (
       
